c371e9fb2b48894c38db74e6aa91d826bd250458
GNataliya <80199130+GNataliya@users.noreply.github.com>
2021-08-04 21:47:36 +0300

    Add files via upload

--- public/js/mainClient.js
+++ public/js/mainClient.js
@@ -1,12 +1,62 @@
 const socket = io();
 
-const allMessages = document.querySelector('.showMessage');
-const userText = document.querySelector('.message');
+const allMessages = document.querySelector('.newMessage');
 const typingMessage = document.querySelector('.typingMessage');
+const userText = document.querySelector('.message');
 
 const formEl = document.forms.sendMessage;
 
-formEl.addEventListener('submit', (ev) => {                        // отслеживаем событие submit и по нажатию срабатывают функции:  
+const getText = (data) => {
+    socket.on('/send text', (data) => {          // получаем сообщения с бека
+    // socket.on('/new text', (data) => {          // получаем сообщения с бека
+    //typingMessage.innerHTML = '';
+    // const html = `<div class="msg">${data.user}:     ${data.message}</div>`;
+    // allMessages.insertAdjacentHTML('beforeend', html); 
+    allMessages.innerHTML += `<div class="msg">${data.user}:     ${data.message}</div>`;  
+   });
+}
+getText();
+
+let timer = null;
+const typingTimer = () => {
+    //typingMessage.classList.remove('show');
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        //typingMessage.classList.add('hidden') 
+        typingMessage.innerHTML = ''
+    }, 1000);
+}
+
+const showTyping = (data) => {
+    socket.on('/typing', (data) => {
+        typingMessage.innerHTML = `<span >${data} is typing a message...</span>`;
+        typingTimer();
+    });
+    
+}
+showTyping();
+
+
+
+
+// let timer = null;
+// const showTyping = (data) => {
+//     socket.on('/typing', (data) => {           // получаем сообщение с бека, что юзер песатает
+//         //console.log(data);
+//         //console.log(data.user);
+//         const typingHtml = `<span >${data} is typing a message...</span>`;
+//         typingMessage.insertAdjacentHTML('beforeend', typingHtml); 
+
+//         clearTimeout(timer);                // очищается, но пока очистится выводит много раз
+//         timer = setTimeout(() => {
+//             typingMessage.innerHTML = ''
+//         }, 500);
+
+//     });
+// }
+
+
+formEl.addEventListener('submit', (ev) => {
     ev.preventDefault();
 
     const formData = new FormData(ev.target);
@@ -15,31 +65,15 @@ formEl.addEventListener('submit', (ev) => {                        // отсле
     const message = formData.get('message');
     
     const sendText = () => {
-        socket.emit('/send text', { user, message });               // отправляем сообщения на бек
-        //console.log({user, message})
-        userText.value = '';                                        // очищаем поле сообщения
+        socket.emit('/send text', { user, message });  // отправляем сообщения на бек
+        userText.value = '';
     }
     sendText();
 
-    userText.addEventListener('keypress', () => {                    // отслеживаем событие keypress 
-        socket.emit('/typing', user)                                 // отправляем эту инфо на сервер
+
+    userText.addEventListener('keypress', () => {
+        socket.emit('/typing', user);
     });
 
-    socket.on('/typing', (data) => {                                  // получаем с сервера инфо о событии typing и выводим ее на фронт 
-        const typingHtml = `<span>${data} is typing a message...</span>`;
-        typingMessage.insertAdjacentHTML('beforeend', typingHtml);
-    });
-
-    const getText = (data) => {
-        socket.on('/new text', (data) => {                             // получаем сообщения с бека
-        //console.log(data)
-        typingMessage.innerHTML = '';                                  //  очищаем поле о наборе текста
-       
-        const html = `<div class="msg">${data.user}:     ${data.message}</div>`;    // выводим сообщение юзерам
-        allMessages.insertAdjacentHTML('beforeend', html)
-       });
-    }
-    getText();
-    
     
 });
